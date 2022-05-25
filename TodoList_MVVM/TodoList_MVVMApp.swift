9db61d5bc6e6b6f0d0f21cdb8e4409782afc7888
andy <a5706617@gmail.com>
2022-05-25 19:37:36 +0800

    Initial Commit

---
+++ TodoList_MVVM/TodoList_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_MVVMApp.swift
+//  TodoList_MVVM
+//
+//  Created by 江啟綸 on 2022/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
